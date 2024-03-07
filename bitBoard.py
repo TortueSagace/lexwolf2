f10411d2d2cd38765222b3d642acae4fb2e1033b
loupwolf <148562941+loupwolf@users.noreply.github.com>
2024-03-07 22:23:22 +0100

    Add files via upload

--- bitBoard.py
+++ bitBoard.py
@@ -1,10 +1,13 @@
 import chess
 import numpy as np
 from weights import WEIGHTS
+import structEl
 
 class bitBoard:
 
     def __init__(self,board=None):
+        self.COLOR_OFFSET = {chess.WHITE: 0, chess.BLACK: 1}
+        self.BOOL_BIN = {True: 1, False: 0}
         self.__list = np.asarray([None]*(768+5)) # private 772-bits bit array: 64*6*2 + 5 ~ 64 cases for each piece of each color plus side to move & castling rights
         self.__pieceswght = np.asarray([None]*(768+5))
         self.setWeights()
@@ -26,20 +29,18 @@ class bitBoard:
 
     def setList(self,board):
             # Dictionary to map piece colors to their index for bitboard representation
-            COLOR_OFFSET = {chess.WHITE: 0, chess.BLACK: 1}
-            BOOL_BIN = {True: 1, False: 0}
             self.__list = np.asarray([0b0]*(768+5))
             for square in chess.SQUARES:
                 piece = board.piece_at(square)
                 if(piece!=None):
-                    index = square + (piece.piece_type-1)*64 + COLOR_OFFSET[piece.color]*64*6
+                    index = square + (piece.piece_type-1)*64 + self.COLOR_OFFSET[piece.color]*64*6
                     self.__list[index] = 0b1
 
-            self.__list[-5] = BOOL_BIN[board.turn]
-            self.__list[-4] = BOOL_BIN[board.has_kingside_castling_rights(chess.WHITE)]
-            self.__list[-3] = BOOL_BIN[board.has_queenside_castling_rights(chess.WHITE)]
-            self.__list[-2] = BOOL_BIN[board.has_kingside_castling_rights(chess.BLACK)]
-            self.__list[-1] = BOOL_BIN[board.has_queenside_castling_rights(chess.BLACK)]
+            self.__list[-5] = self.BOOL_BIN[board.turn]
+            self.__list[-4] = self.BOOL_BIN[board.has_kingside_castling_rights(chess.WHITE)]
+            self.__list[-3] = self.BOOL_BIN[board.has_queenside_castling_rights(chess.WHITE)]
+            self.__list[-2] = self.BOOL_BIN[board.has_kingside_castling_rights(chess.BLACK)]
+            self.__list[-1] = self.BOOL_BIN[board.has_queenside_castling_rights(chess.BLACK)]
 
     def setWeights(self):        
         self.__pieceswght[0:64] = np.asarray([WEIGHTS['PAWN_VALUE']]*64)

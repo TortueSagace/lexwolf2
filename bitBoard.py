5ca9dde6194167b9652fd291c2d50e69d6962a7a
loupwolf <148562941+loupwolf@users.noreply.github.com>
2024-03-09 23:32:57 +0100

    Add files via upload

--- bitBoard.py
+++ bitBoard.py
@@ -1,22 +1,23 @@
 import chess
 import numpy as np
 from weights import WEIGHTS
-import structEl
+from structEl import structEl
 
 class bitBoard:
 
     def __init__(self,board=None):
         self.COLOR_OFFSET = {chess.WHITE: 0, chess.BLACK: 1}
         self.BOOL_BIN = {True: 1, False: 0}
+        self.board = board
         self.__list = np.asarray([None]*(768+5)) # private 772-bits bit array: 64*6*2 + 5 ~ 64 cases for each piece of each color plus side to move & castling rights
         self.__pieceswght = np.asarray([None]*(768+5))
         self.setWeights()
+        self.strEl = structEl()
 
         if(board==None): # no board provided, initial board is supposed
-            temp = chess.Board()
-            self.setList(temp)
-        elif(board!=None):
-            self.setList(board)
+            self.board = chess.Board()
+
+        self.setList(self.board)
 
     def getList(self):
         return self.__list
@@ -25,10 +26,122 @@ class bitBoard:
         return self.__list*self.__pieceswght
 
     def getEval(self):
-        return np.sum(self.getWeighted())
+        a1 = 2/3 # material
+        a2 = (1-a1)/3 # b ctl
+        a3 = (1-a1)/3 # q ctl
+        a4 = (1-a1)/3 # r ctl
+        return a1*np.sum(self.getWeighted()) + a2*self.bishopCtl() + a3*self.queenCtl() + a4*self.rookCtl()
+    
+    def bishopCtl(self):
+        res = 0
+        al = 1/2 # allied control
+        en = 1/2 # ennemy control
+        weighted = self.getWeighted()
+        if(self.board.turn == chess.WHITE):
+            bishops = self.__list[2*64:3*64]
+            weighted[2*64:3*64] = np.asarray([0]*64) 
+            sponge = self.sponge(weighted)
+            allied = (sponge + abs(sponge))/2
+            ennemy = (-sponge + abs(sponge))/2
+            conva = np.asarray(64*[0])
+            conve = np.asarray(64*[0])
+            for i in range(64):
+                conva[i] = self.strEl.Bp[i]@allied
+                conve[i] = self.strEl.Bp[i]@ennemy
+            res = (al*conva+en*conve)@bishops
 
+        else:    
+            bishops = self.__list[8*64:9*64]
+            weighted[8*64:9*64] = np.asarray([0]*64)             
+            sponge = self.sponge(weighted)
+            allied = (-sponge + abs(sponge))/2
+            ennemy = (sponge + abs(sponge))/2
+            conva = np.asarray(64*[0])
+            conve = np.asarray(64*[0])
+            for i in range(64):
+                conva[i] = self.strEl.Bp[i]@allied
+                conve[i] = self.strEl.Bp[i]@ennemy
+            res = (al*conva+en*conve)@bishops
+
+        return res
+    
+    def queenCtl(self): 
+        res = 0
+        al = 1/2 # allied control
+        en = 1/2 # ennemy control
+        weighted = self.getWeighted()
+        if(self.board.turn == chess.WHITE):
+            queen = self.__list[4*64:5*64]
+            weighted[4*64:5*64] = np.asarray([0]*64) 
+            sponge = self.sponge(weighted)
+            allied = (sponge + abs(sponge))/2
+            ennemy = (-sponge + abs(sponge))/2
+            conva = np.asarray(64*[0])
+            conve = np.asarray(64*[0])
+            for i in range(64):
+                conva[i] = self.strEl.Qn[i]@allied
+                conve[i] = self.strEl.Qn[i]@ennemy
+            res = (al*conva+en*conve)@queen
+
+        else:    
+            queen = self.__list[10*64:11*64]
+            weighted[10*64:11*64] = np.asarray([0]*64)         
+            sponge = self.sponge(weighted)
+            allied = (-sponge + abs(sponge))/2
+            ennemy = (sponge + abs(sponge))/2
+            conva = np.asarray(64*[0])
+            conve = np.asarray(64*[0])
+            for i in range(64):
+                conva[i] = self.strEl.Qn[i]@allied
+                conve[i] = self.strEl.Qn[i]@ennemy
+            res = (al*conva+en*conve)@queen
+            
+        return res 
+
+    def rookCtl(self):      
+        res = 0
+        al = 1/2 # allied control
+        en = 1/2 # ennemy control
+        weighted = self.getWeighted()
+        if(self.board.turn == chess.WHITE):
+            rooks = self.__list[3*64:4*64]
+            weighted[3*64:4*64] = np.asarray([0]*64) 
+            sponge = self.sponge(weighted)
+            allied = (sponge + abs(sponge))/2
+            ennemy = (-sponge + abs(sponge))/2
+            conva = np.asarray(64*[0])
+            conve = np.asarray(64*[0])
+            for i in range(64):
+                conva[i] = self.strEl.Rk[i]@allied
+                conve[i] = self.strEl.Rk[i]@ennemy
+            res = (al*conva+en*conve)@rooks
+
+        else:    
+            rooks = self.__list[9*64:10*64]
+            weighted[9*64:10*64] = np.asarray([0]*64)         
+            sponge = self.sponge(weighted)
+            allied = (-sponge + abs(sponge))/2
+            ennemy = (sponge + abs(sponge))/2
+            conva = np.asarray(64*[0])
+            conve = np.asarray(64*[0])
+            for i in range(64):
+                conva[i] = self.strEl.Rk[i]@allied
+                conve[i] = self.strEl.Rk[i]@ennemy
+            res = (al*conva+en*conve)@rooks
+            
+        return res     
+    
+    def sponge(self, array=None):
+        res = [0]*64
+        if(not array.any()):
+            array = self.__list
+        for i in range(12):
+            res+=array[i*64:(i+1)*64]    
+        return res
+    
     def setList(self,board):
             # Dictionary to map piece colors to their index for bitboard representation
+            self.board = board
             self.__list = np.asarray([0b0]*(768+5))
             for square in chess.SQUARES:
                 piece = board.piece_at(square)
